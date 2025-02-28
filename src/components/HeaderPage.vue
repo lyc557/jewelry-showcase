f324325600182a25ad17c16eb270a4fd3877c638
email <lyc557@163.com>
2025-03-01 00:05:38 +0800

    修改牌和logo的布局

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,13 +1,17 @@
 <template>
   <header class="header">
     <nav>
-      <div class="logo">奢华珠宝</div>
-      <ul class="nav-links">
-        <li><router-link to="/">首页</router-link></li>
-        <li><router-link to="/collections">系列</router-link></li>
-        <li><router-link to="/about">品牌故事</router-link></li>
-        <li><router-link to="/contact">联系我们</router-link></li>
-      </ul>
+      <div class="logo-container">
+        <div class="logo"></div>
+        <div class="logo-text">祺合珠宝</div>
+
+        <ul class="nav-links">
+          <li><router-link to="/">首页</router-link></li>
+          <li><router-link to="/collections">系列</router-link></li>
+          <li><router-link to="/about">品牌故事</router-link></li>
+          <li><router-link to="/contact">联系我们</router-link></li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
@@ -17,6 +21,12 @@
   background: #fff;
   box-shadow: 0 2px 4px rgba(0,0,0,0.1);
   padding: 1rem 2rem;
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 }
 
 .nav-links {
